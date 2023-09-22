f0f20a24c0879dae6ab8f8774cb6a5dd6e2dfded
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:02:07 +0300

    Update and rename mul.c to fc_mul.c

--- c_files/fc_mul.c
+++ c_files/fc_mul.c
@@ -1,11 +1,12 @@
 #include "monty.h"
+
 /**
- * f_mul - multiplies the top two elements of the stack.
+ * fc_mul - multiplies the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
-void f_mul(stack_t **head, unsigned int counter)
+void fc_mul(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
@@ -18,7 +19,7 @@ void f_mul(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't multiply, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
