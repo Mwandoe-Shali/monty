971d97aca468ac311821b2827841910738aafcb6
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:12:44 +0300

    Update and rename push.c to fc_push.c

--- c_files/fc_push.c
+++ c_files/fc_push.c
@@ -1,11 +1,12 @@
 #include "monty.h"
+
 /**
- * f_push - add node to the stack
+ * fc_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void fc_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
 
@@ -31,7 +32,7 @@ void f_push(stack_t **head, unsigned int counter)
 		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
-		addnode(head, n);
+		fc_addnode(head, n);
 	else
-		addqueue(head, n);
+		fc_addqueue(head, n);
 }
