8ad09296a4b8f355babd96ae58bfa24141dbf182
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:32:13 +0300

    Update fc_mod.c

--- c_files/fc_mod.c
+++ c_files/fc_mod.c
@@ -2,10 +2,10 @@
 
 /**
  * fc_mod - computes the rest of the division of the second
- * top element of the stack by the top element of the stack
+ *		top element of the stack by the top element of the stack
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
 void fc_mod(stack_t **head, unsigned int counter)
 {
