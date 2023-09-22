1ecc77ccce07008662942f711a76165badfc3a94
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:19:57 +0300

    Update rotr.c

--- c_files/rotr.c
+++ c_files/rotr.c
@@ -1,11 +1,12 @@
 #include "monty.h"
+
 /**
-  *f_rotr- rotates the stack to the bottom
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
+  * fc_rotr- rotates the stack to the bottom
+  * @head: stack head
+  * @counter: line_number
+  * Return: void
  */
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+void fc_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
 	stack_t *copy;
 
