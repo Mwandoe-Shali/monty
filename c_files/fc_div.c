b42c88a364c8253ef1fca10165dd49986291cd11
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-22 23:29:05 +0300

    Update and rename div.c to fc_div.c

--- c_files/fc_div.c
+++ c_files/fc_div.c
@@ -1,11 +1,12 @@
 #include "monty.h"
+
 /**
- * f_div - divides the top two elements of the stack.
+ * fc_div - divides the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
-void f_div(stack_t **head, unsigned int counter)
+void fc_div(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
