8b35455233052f3215bb6dc4c47ec381547c3169
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:03:54 +0300

    Update and rename nop.c to fc_nop.c

---
+++ c_files/fc_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  * fc_nop- nothing
+  * @head: stack head
+  * @counter: line_number
+  * Return: void
+ */
+void fc_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
