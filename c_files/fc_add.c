76bf77461e688cb1774395d101bde4f5511898f4
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-22 23:25:35 +0300

    Update and rename add.c to fc_add.c

--- c_files/fc_add.c
+++ c_files/fc_add.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * fc_add - adds the top two elements of the stack.
+ * @head: stack's head
+ * @counter: the number of the line
+ * Return: void
 */
-void f_add(stack_t **head, unsigned int counter)
+void fc_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
