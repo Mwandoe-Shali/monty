42b33771f8973b7e087f8c5a88234c70eb9e28cd
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-23 00:09:18 +0300

    Update and rename pop.c to fc_pop.c

--- c_files/fc_pop.c
+++ c_files/fc_pop.c
@@ -1,17 +1,18 @@
 #include "monty.h"
+
 /**
- * f_pop - prints the top
+ * fc_pop - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_pop(stack_t **head, unsigned int counter)
+void fc_pop(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fprintf(stderr, "L%d: can't pop an empty stack.\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
