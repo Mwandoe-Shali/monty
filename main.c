5b45fb6d8b0a1a54c7e3df551608c7dfa1932fd0
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-24 14:29:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * main - entry point
  * @argc: arguments count
  * @argv: list of arguments
- * 
+ *
  * Return: always 0
  */
 
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 /**
  * create_node - Creates a node.
  * @n: Number to go inside the node.
- * 
+ *
  * Return: Upon sucess a pointer to the node. Otherwise NULL.
  */
 stack_t *create_node(int n)
@@ -41,7 +41,7 @@ stack_t *create_node(int n)
 
 /**
  * free_nodes - Frees nodes in the stack.
- * 
+ *
  * Return: Void
  */
 void free_nodes(void)
@@ -64,7 +64,7 @@ void free_nodes(void)
  * add_to_queue - Adds a node to the queue.
  * @new_node: Pointer to the new node.
  * @ln: line number of the opcode.
- * 
+ *
  * Return: Void
  */
 void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
