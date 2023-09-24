ea07b98529dee50cdcd2d2239f08637e182564e9
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-24 14:41:06 +0300

    Update stack_opcode_A.c

--- stack_opcode_A.c
+++ stack_opcode_A.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * add_to_stack - Adds a node to the stack.(func push)
+ * add_to_stack - Adds a node to the stack. (func push)
  * @new_node: Pointer to the new node.
- * @ln: Interger representing the line number of of the opcode.
+ * @ln: Integer representing the line number of the opcode.
  *
  * Return: Void
  */
@@ -26,8 +26,8 @@ void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 
 
 /**
- * print_stack - Prints nodes of the stack.(func pall)
- * @stack: Pointer to a pointer pointing to top node of the stack.
+ * print_stack - Prints nodes of the stack. (func pall)
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: line number of  the opcode.
  *
  * Return: void
@@ -48,9 +48,9 @@ void print_stack(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * pop_top - Adds a node to the stack.(func pop)
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * pop_top - Adds a node to the stack. (func pop)
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Integer representing the line number of the opcode.
  *
  * Return: Void
  */
@@ -69,9 +69,9 @@ void pop_top(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * print_top - Prints the top node of the stack.(func pint)
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * print_top - Prints the top node of the stack. (func pint)
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Integer representing the line number of the opcode.
  *
  * Return: Void
  */
