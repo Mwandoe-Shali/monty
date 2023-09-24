1c14efafd2ccde83326b99e47b5ef58c142e7752
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-24 14:28:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-stack_t *head = NULL;
 
 /**
  * main - entry point
@@ -48,6 +47,7 @@ stack_t *create_node(int n)
 void free_nodes(void)
 {
 	stack_t *tmp;
+	stack_t *head = NULL;
 
 	if (head == NULL)
 		return;
@@ -70,6 +70,7 @@ void free_nodes(void)
 void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
 	stack_t *tmp;
+	stack_t *head = NULL;
 
 	if (new_node == NULL || *new_node == NULL)
 		exit(EXIT_FAILURE);
